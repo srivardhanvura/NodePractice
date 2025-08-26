35cff31219dfce427832b71e6ac76335f69510b4
Srivardhan <srivardhan.vura@lji.io>
2025-08-26 00:08:06 -0500

    git ignore

--- 7-promise.js
+++ 7-promise.js
@@ -1,15 +1,11 @@
-const {readFile, writeFile} = require('fs');
-const util = require('util');
-
-const readFilePromise = util.promisify(readFile)
-const writeFilePromise = util.promisify(writeFile)
+const {readFile, writeFile} = require('fs').promises
 
 const start = async () => {
     try{
-        const first = await readFilePromise('./content/first.txt', 'utf8');
-        const second = await readFilePromise('./content/second.txt', 'utf8');
+        const first = await readFile('./content/first.txt', 'utf8');
+        const second = await readFile('./content/second.txt', 'utf8');
 
-        await writeFilePromise('./content/result-promise.txt', `${first} ${second}`)
+        await writeFile('./content/result-promise.txt', `${first} ${second}`)
 
         console.log(`${first} ${second}`)
     }
@@ -17,6 +13,27 @@ const start = async () => {
         console.log(err)
     }
 }
+
+
+// const {readFile, writeFile} = require('fs');
+// const util = require('util');
+
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
+
+// const start = async () => {
+//     try{
+//         const first = await readFilePromise('./content/first.txt', 'utf8');
+//         const second = await readFilePromise('./content/second.txt', 'utf8');
+
+//         await writeFilePromise('./content/result-promise.txt', `${first} ${second}`)
+
+//         console.log(`${first} ${second}`)
+//     }
+//     catch(err){
+//         console.log(err)
+//     }
+// }
 
 
 start()
